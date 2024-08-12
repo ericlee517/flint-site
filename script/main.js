a7b6e9b2b26a38478e2c8983fb5ebad34bc51417
ericlee517 <3420381303@qq.com>
2024-08-12 14:38:08 +0800

    v4.5-240813 commit

--- script/main.js
+++ script/main.js
@@ -8,4 +8,52 @@ fetch("/json/message.json") //json文件
             document.getElementById("msg").innerHTML = data.message
         }
     });
+    
+    
+function showError(){
 
+Toastify({
+  text: "该功能暂未上线。",
+  duration: 3000,
+  newWindow: true,
+  gravity: "top", // `top` or `bottom`
+  position: "center", // `left`, `center` or `right`
+  stopOnFocus: true, // Prevents dismissing of toast on hover
+  style: {
+    background: "linear-gradient(to right, #f78ca0 0%, #f9748f 19%, #fd868c 60%, #fe9a8b 100%)",
+  },
+  onClick: function(){} // Callback after click
+}).showToast();
+    
+}
+
+function showClose(){
+
+Toastify({
+  text: "活动暂未开启。",
+  duration: 3000,
+  newWindow: true,
+  gravity: "top", // `top` or `bottom`
+  position: "center", // `left`, `center` or `right`
+  stopOnFocus: true, // Prevents dismissing of toast on hover
+  style: {
+    background: "linear-gradient(to right, #f78ca0 0%, #f9748f 19%, #fd868c 60%, #fe9a8b 100%)",
+  },
+  onClick: function(){} // Callback after click
+}).showToast();
+    
+}
+
+function showNolink(){
+
+Toastify({
+  text: "当前链接没有重定向的目标。",
+  duration: 3000,
+  newWindow: true,
+  gravity: "top", // `top` or `bottom`
+  position: "center", // `left`, `center` or `right`
+  stopOnFocus: true, // Prevents dismissing of toast on hover
+  onClick: function(){} // Callback after click
+}).showToast();
+    
+}
