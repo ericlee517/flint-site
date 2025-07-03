792c3e91ae51fe84142550e167357a319d8b05ca
ericlee517 <3420381303@qq.com>
2025-07-03 09:33:52 +0800

    20250703 update

--- themes/flint/source/js/script.js
+++ themes/flint/source/js/script.js
@@ -1,6 +1,6 @@
 /*
  * the main javascript
- * lastest update: 2025/04/29
+ * lastest update: 2025/07/02
  * author: ericlee517
  * description: basic for the tags to comfirm with tailwindcss.
  */
@@ -29,15 +29,67 @@ headings.forEach((heading) => {
   }
 });
 
-  // 页面加载完成后执行
-window.addEventListener('load', function() {
-  const tooltip = document.getElementById('tooltip');
-  // 显示tooltip，添加class tooltip-open
-  tooltip.classList.add('tooltip-open');
-  // 七秒后隐藏tooltip，移除class tooltip-open
+window.addEventListener("load", function () {
+  const tooltip = document.getElementById("tooltip");
+  tooltip.classList.add("tooltip-open");
   setTimeout(() => {
-    tooltip.classList.remove('tooltip-open');
+    tooltip.classList.remove("tooltip-open");
   }, 7000);
 
-    window.loadTime = Date.now();
-  });
+  window.loadTime = Date.now();
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const timeElement = document.querySelector("[data-time]");
+  if (!timeElement) {
+    console.error("[turboflint]data-time is empty");
+    return;
+  }
+  const convertTimeFormat = (timeStr) => {
+    if (!timeStr) return null;
+    return timeStr.replace(/-(\d{2}:\d{2}:\d{2})$/, "T$1");
+  };
+  const rawTime = timeElement.getAttribute("data-time");
+  const targetDate = new Date(convertTimeFormat(rawTime));
+  if (isNaN(targetDate.getTime())) {
+    console.error("[turboflint]Unable to parse the incoming time format. Please ensure the format is YYYY-MM-DD-hh:mm:ss");
+    return;
+  }
+
+  const updateCountdown = function () {
+    const currentTime = new Date();
+    if (targetDate < currentTime) {
+      const daysElement = document.getElementById("countdown-days");
+      const hoursElement = document.getElementById("countdown-hours");
+      const minutesElement = document.getElementById("countdown-minutes");
+      const secondsElement = document.getElementById("countdown-seconds");
+
+      if (daysElement && hoursElement && minutesElement && secondsElement) {
+        daysElement.textContent = 0;
+        hoursElement.textContent = 0;
+        minutesElement.textContent = 0;
+        secondsElement.textContent = 0;
+      }
+      return;
+    }
+
+    const timespan = countdown(
+      currentTime,
+      targetDate,
+      countdown.DAYS | countdown.HOURS | countdown.MINUTES | countdown.SECONDS
+    );
+    const daysElement = document.getElementById("countdown-days");
+    const hoursElement = document.getElementById("countdown-hours");
+    const minutesElement = document.getElementById("countdown-minutes");
+    const secondsElement = document.getElementById("countdown-seconds");
+
+    if (daysElement && hoursElement && minutesElement && secondsElement) {
+      daysElement.textContent = timespan.days;
+      hoursElement.textContent = timespan.hours;
+      minutesElement.textContent = timespan.minutes;
+      secondsElement.textContent = timespan.seconds;
+    }
+  };
+  updateCountdown();
+  setInterval(updateCountdown, 1000);
+});
